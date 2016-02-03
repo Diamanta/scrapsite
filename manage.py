0d1f35507ec2fb0b5335244ba883e18ec88f1b23
diamanta <diamanta.2084@gmail.com>
2016-02-03 22:55:32 +0300

    Initial commit

---
+++ manage.py
@@ -0,0 +1,10 @@
+#!C:\Python 35
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrapsite.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
